f78506304c2a4e6a93836230ca341eed2e61824b
PPableo <pableop@gmail.com>
2022-09-29 15:40:09 -0700

    set-up folder and file structures

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-Project 2
+// Project 2
 
 // Create an app object (wineParing)
 
@@ -18,3 +18,11 @@ Project 2
         // - add a 'change' event listener to call the local method (wineChoice), to track user input
 
 // ** used pseudo code example as a template: https://github.com/HackerYou/bootcamp-notes/blob/main/projects/project-02/README.md#pseudo-code ** //
+
+fetch('https://api.spoonacular.com/food/wine/pairing?apiKey=8161e734602a4734b2d6d521776cfb99&food=cheese')
+.then( (apiData)=>{
+    return apiData.json();
+})
+.then( function(apiData){
+    console.log(`Recipe Data`, apiData);
+})
