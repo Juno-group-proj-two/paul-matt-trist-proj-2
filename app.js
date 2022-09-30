913b7dabe9c9044c2de2981a4d79df1403546ff6
mattvl <mvamlux@gmail.com>
2022-09-30 15:41:21 -0400

    updated js

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-fetch('https://api.spoonacular.com/food/wine/pairing?apiKey=8161e734602a4734b2d6d521776cfb99&food=burger')
-.then( (apiData)=>{
-    return apiData.json();
-})
-.then( function(apiData){
-    console.log(`Recipe Data`, apiData);
-})
+const getUserInput = () => {
+    const userForm = document.getElementById('wineForm');
+    userForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        const select = document.getElementById('wineOptions');
+        const userInput = select.options[select.selectedIndex].value;
+            console.log(userInput);
+        getDishes(userInput)
+    });
+}
+getUserInput();
+
+const getDishes = (wine) => {
+    fetch(`https://api.spoonacular.com/food/wine/dishes?apiKey=8161e734602a4734b2d6d521776cfb99&wine=${wine}`)
+    .then( (apiData)=>{
+        return apiData.json();
+    })
+    .then( function(apiData){
+        console.log(`Recipe Data`, apiData);
+    })
+
+} 
+
+
