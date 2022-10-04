c80e4cb5c99a1ea0d34744c26fa1228b466fdded
PPableo <pableop@gmail.com>
2022-10-04 01:20:29 -0400

    JS logic - error handling - first iteration

--- scripts.js
+++ scripts.js
@@ -19,10 +19,42 @@
 
 // ** used pseudo code example as a template: https://github.com/HackerYou/bootcamp-notes/blob/main/projects/project-02/README.md#pseudo-code ** //
 
-fetch('https://api.spoonacular.com/food/wine/pairing?apiKey=8161e734602a4734b2d6d521776cfb99&food=cheese')
-.then( (apiData)=>{
-    return apiData.json();
-})
-.then( function(apiData){
-    console.log(`Recipe Data`, apiData);
-})
+// fetch('https://api.spoonacular.com/food/wine/pairing?apiKey=8161e734602a4734b2d6d521776cfb99&food=cheese')
+// .then( (apiData)=>{
+//     return apiData.json();
+// })
+// .then( function(apiData){
+//     console.log(`Recipe Data`, apiData);
+// })
+
+// *** Wine with Paired Dish ***
+const url1 = "https://api.spoonacular.com/food/wine/dishes?apiKey=897d5fbeefc34f42adb50cfbbfb70ac9&wine=riesling"
+// *** Wine with NO! Paired Dish ***
+const url2 = "https://api.spoonacular.com/food/wine/dishes?apiKey=897d5fbeefc34f42adb50cfbbfb70ac9&wine=sauternes"
+// *** Wine ERROR ***
+const url3 = "https://api.spoonacular.com/food/wine/dishes?apiKey=897d5fbeefc34f42adb50cfbbfb70ac9&wine=break"
+
+fetch(url1)
+    .then((response) => {
+        console.log(response);
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.ok)
+        }
+    })
+    .then((data) => {
+        console.log(data);
+        if (data.status === "failure") {
+            alert("To be Updated, Try Again!")
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+
+        if (error.message === "false") {
+            alert("Please Select a Wine in the Dropdown Menu")
+        } else {
+            alert("Try again!")
+        }
+    });
