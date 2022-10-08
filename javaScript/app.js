f7e0b4043e87b4bcc6fc5ef5e9bf493beac1591f
mattvl <mvamlux@gmail.com>
2022-10-08 15:51:46 -0400

    worked on hamburger menu

--- javaScript/app.js
+++ javaScript/app.js
@@ -123,3 +123,10 @@ app.init = () => {
 };
 
 app.init();
+
+const toggleButton = document.getElementsByClassName("toggleButton")[0]
+const navBarLinks = document.getElementsByClassName("navBarLinks")[0]
+
+toggleButton.addEventListener ('click', () => {
+    navBarLinks.classList.toggle('active');
+});
